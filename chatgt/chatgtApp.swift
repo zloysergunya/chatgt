54884247619bef0ceea6db03d9a73ad7d2599013
zloysergunya <zloysergunya@mail.ru>
2025-12-07 21:04:21 +0300

    Initial Commit

---
+++ chatgt/chatgtApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatgtApp.swift
+//  chatgt
+//
+//  Created by Sergey Kotov on 07.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct chatgtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
